825c1bbc4381c6762201077da0f3c7bdca6f3bf4
neevan-gta99 <naveengta8581@gmail.com>
2025-12-22 12:00:23 +0530

    update server js 2

--- server.js
+++ server.js
@@ -7,13 +7,38 @@ dotenv.config();
 
 const port = process.env.PORT || 8900;
 
+let redisClient;
+
 (async () => {
-  const redisClient = await redisConnection();
+  redisClient = await redisConnection();
   await connectDB();
-
-  app.locals.redisClient = redisClient; // attach to app for global use
-
-  app.listen(port, () => {
-    console.log(`Server is Running: localhost:${port}`);
-  });
 })();
+
+app.listen(port,()=>{
+    console.log(`Server is Running: localhost:${port}`);
+})
+
+export {redisClient};
+
+
+// import app from './app.js';
+// import dotenv from 'dotenv';
+// import connectDB from './DB.js';
+// import redisConnection from './redis/connection.js';
+
+// dotenv.config();
+
+// const port = process.env.PORT || 8900;
+
+// (async () => {
+//   const redisClient = await redisConnection();
+//   await connectDB();
+
+//   app.locals.redisClient = redisClient; // attach to app for global use
+
+//   app.listen(port, () => {
+//     console.log(`Server is Running: localhost:${port}`);
+//   });
+// })();
+
+// export default { redisClient };
