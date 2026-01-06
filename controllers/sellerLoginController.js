d9b83258c86789f58941deea927973bcf40d053a
neevan-gta99 <naveengta8581@gmail.com>
2026-01-06 16:41:20 +0530

    Update sellerLoginController.js

--- controllers/sellerLoginController.js
+++ controllers/sellerLoginController.js
@@ -6,7 +6,7 @@ export const sellerLogin = async (req, res) => {
    const authRes = await seller_Login_Model.checkCred(req,res);
 
    // res.status(authRes.code).json({message: authRes.message,userData: authRes.userData});
-   res.status(authRes.code).json({message: authRes.message,token: authRes.jwt_token});
+   res.status(authRes.code).json({message: authRes.message,token: authRes.jwt_token, sellerId: authRes.sellerId});
 
    // console.log(authRes.code,authRes.message,authRes.jwt_token);
 
